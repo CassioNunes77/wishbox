4507290604e9caefe111761b15a260bcfb982068
CassioNunes77 <cassionunes.si@gmail.com>
2025-12-02 20:39:23 -0300

    Initial Commit

---
+++ WishBoxTests/WishBoxTests.swift
@@ -0,0 +1,16 @@
+//
+//  WishBoxTests.swift
+//  WishBoxTests
+//
+//  Created by Cássio Nunes on 02/12/25.
+//
+
+import Testing
+
+struct WishBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
